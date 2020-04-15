6bf2f91f3f565cd2d8f95d1e1373c171c7258843
Mateusz Zajac <TRBC37@motorolasolutions.com>
2020-04-15 16:43:32 +0200

    display list of product in client site

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SkiNet Shop';
+  products: any[];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<IProduct>('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
